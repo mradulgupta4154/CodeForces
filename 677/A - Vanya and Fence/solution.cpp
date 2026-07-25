5339fe23c59af6af69af1e429758e482a81035a0
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-25 13:23:49 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int n,h;
+    cin>>n>>h;
+    int width=0;
+    for(int i =0;i<n;i++){
+        int a ;
+        cin>>a;
+        width+=(a>h)?2:1;
+    }
+    cout<<width;  
+    return 0;
+}
