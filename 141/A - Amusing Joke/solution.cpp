c5b2c86e5bb5cea63c2622c802cd80a60c938b86
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-27 15:54:51 +0530

    Add Amusing Joke [A] from Codeforces

---
+++ 141/A - Amusing Joke/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string a,b,c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    string d = a+b;
+ 
+    sort(d.begin(),d.end());
+    sort(c.begin(),c.end());
+    if(c==d) cout<<"YES";
+    else cout<<"NO";
+    
+}
