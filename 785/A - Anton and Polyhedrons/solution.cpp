b961d93c40dc7c87162f01f225062d05ed7f46d8
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-27 15:22:17 +0530

    Add Anton and Polyhedrons [A] from Codeforces

---
+++ 785/A - Anton and Polyhedrons/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int n ;
+    cin>>n;
+    long long sum=0;
+    while(n--){
+        string a;
+        cin>>a;
+        if(a=="Tetrahedron"){
+            sum+=4;
+        }
+        else if(a=="Cube"){
+            sum+=6;
+        }
+        else if(a=="Octahedron"){
+            sum+=8;
+        }
+        else if(a=="Dodecahedron"){
+            sum+=12;
+        }
+        else{
+            sum+=20;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
