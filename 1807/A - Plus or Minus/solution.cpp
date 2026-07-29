683d5a356e3bd7b7b6924a30548e405fcd5894d6
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-29 15:17:07 +0530

    Add Plus or Minus [A] from Codeforces

---
+++ 1807/A - Plus or Minus/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    int n ;
+    cin>>n;
+    while(n--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c) cout<<"+"<<endl;
+        if(a-b==c) cout<<"-"<<endl;
+        
+    }
+ 
+}
