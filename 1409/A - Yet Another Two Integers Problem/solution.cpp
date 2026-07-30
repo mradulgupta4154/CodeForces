201d1169449cb5b91e60f047462458193d0a197f
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-30 14:49:59 +0530

    Add Yet Another Two Integers Problem [A] from Codeforces

---
+++ 1409/A - Yet Another Two Integers Problem/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+#include <unordered_map>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        cout<<(abs(a-b)+9)/10<<endl;
+        
+        
+    }
+    
+    return 0;
+ 
+}
