5c54ad71546272c31219c5fa041bf1c5a0ecd3fe
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-30 14:51:23 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 1915/A - Odd One Out/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+#include <unordered_map>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int ans=a^b^c;
+        cout<<ans<<endl;
+        
+        
+        
+    }
+    
+    return 0;
+ 
+}
