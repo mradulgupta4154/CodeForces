9dfaf2325527ae4a2a4004ba11a70652c638b421
Mradul gupta <mradulgupta387a@gmail.com>
2026-07-31 17:04:19 +0530

    Add Mishka and Game [A] from Codeforces

---
+++ 703/A - Mishka and Game/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int m=0,c=0;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        if(a>b) m++;
+        else if(a<b)c++;
+    }
+    if(m>c) cout<<"Mishka"<<endl;
+    else if(m<c)cout<<"Chris"<<endl;
+    else cout<<"Friendship is magic!^^"<<endl;
+    return 0;
+}
