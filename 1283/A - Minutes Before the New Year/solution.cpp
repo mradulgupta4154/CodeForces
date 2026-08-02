8521b9377a448034eeccaa7756b1c2c16a460810
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-02 13:31:52 +0530

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<24*60-(a*60+b)<<endl;
+        
+ 
+ 
+    }
+ 
+    return 0;
+}
