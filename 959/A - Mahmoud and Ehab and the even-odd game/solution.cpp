4f6820c5d366ee06e0e649d4d9fe676dc5863dcd
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-04 16:00:41 +0530

    Add Mahmoud and Ehab and the even-odd game [A] from Codeforces

---
+++ 959/A - Mahmoud and Ehab and the even-odd game/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include<string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    if(t%2==0){
+        cout<<"Mahmoud"<<endl;
+ 
+    }
+    else{
+        cout<<"Ehab"<<endl;
+    }
+    
+    return 0;
+ 
+ 
+}
