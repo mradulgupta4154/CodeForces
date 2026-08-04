ce87c5e2113b641dddab3c88597677f472f5c3f7
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-04 14:58:54 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+void solve() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    
+ 
+    cout << s1[0] << s2[0] << s3[0] << "
+";
+}
+ 
+int main() {
+ 
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
