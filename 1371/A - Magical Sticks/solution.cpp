5ca40b024a7707194bf302406baff8a35cb5e429
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-06 16:27:12 +0530

    Add Magical Sticks [A] from Codeforces

---
+++ 1371/A - Magical Sticks/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+void solve() {
+    long long n;
+    cin >> n;
+    cout << (n + 1) / 2 << "
+";
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
