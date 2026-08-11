053489c7916819d9552eacc38f490d515151d138
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-11 15:51:40 +0530

    Add Rudolf and the Ugly String [C] from Codeforces

---
+++ 1941/C - Rudolf and the Ugly String/solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+ 
+    int ans = 0;
+    int i = 0;
+    while (i < n) {
+        if (i + 4 < n && s.substr(i, 5) == "mapie") {
+            ans++;
+            i += 5;
+        } else if (i + 2 < n && (s.substr(i, 3) == "map" || s.substr(i, 3) == "pie")) {
+            ans++;
+            i += 3;
+        } else {
+            i++;
+        }
+    }
+ 
+    cout << ans << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
