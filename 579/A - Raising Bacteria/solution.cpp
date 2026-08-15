29d4baa8be098df76077eba60bc8d23e1781ba75
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-15 15:13:07 +0530

    Add Raising Bacteria [A] from Codeforces

---
+++ 579/A - Raising Bacteria/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n > 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+    cout << count << endl;
+    return 0;
+}
