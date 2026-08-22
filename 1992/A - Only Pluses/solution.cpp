a875ec905705e83444b09f5033109cfc50b14cda
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-22 17:26:49 +0530

    Add Only Pluses [A] from Codeforces

---
+++ 1992/A - Only Pluses/solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    for (int i = 0; i < 5; ++i) {
+        if (a <= b && a <= c) {
+            a++;
+        } else if (b <= a && b <= c) {
+            b++;
+        } else {
+            c++;
+        }
+    }
+    
+    cout << a * b * c << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
