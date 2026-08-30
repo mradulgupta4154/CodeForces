3d4273b3be56fae09ad8f538bbf6c5fbd8e8b17f
Mradul gupta <mradulgupta387a@gmail.com>
2026-08-30 15:33:21 +0530

    Add Ichihime and Triangle [A] from Codeforces

---
+++ 1337/A - Ichihime and Triangle/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<numeric>
+using namespace std;
+ 
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int b,c,d,e;
+        cin>>b>>c>>d>>e;
+        int x,y,z;
+        x=c;
+        y=d;
+        z=d;
+        cout<<x<<" "<<y<<" "<<z<<endl;
+    }
+    return 0;
+}
